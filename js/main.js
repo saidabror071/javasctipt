97e716099a77d2ae2971fb62638d556b767eaf3b
saidabror071 <yusupovsaidabror96@gmail.com>
2023-12-14 15:40:00 +0300

    intitla commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var Assalomu = prompt("Assalomu alaykum")
+console.log("assalamu alaykum");
+var isimingiz = prompt("Ismingizni kiriting!")
+console.log("isimingizni kiriting!");
+var tel = prompt("Telefon Nomeringizni kiriting!")
+console.log("telefon nomeringizni kiriting!");
+var yosh = prompt("Yoshingizni kiriting!")
+console.log("yoshshingizni kiriting!");
